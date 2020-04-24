c65e7368c6af1bcf8c90ed638d196d9a7acd2ac8
Ilya <RashnFlow@yandex.ru>
2020-04-24 21:54:10 +0300

    Initial commit

---
+++ android/src/ru/shishmakov/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.shishmakov;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.shishmakov.StarShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarShooter(), config);
+	}
+}
