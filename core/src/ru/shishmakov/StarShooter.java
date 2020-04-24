56b7c7d4b11ee7fc4e16cc21c78ca9bc8c637aff
Ilya <RashnFlow@yandex.ru>
2020-04-24 22:15:56 +0300

    homework1

--- core/src/ru/shishmakov/StarShooter.java
+++ core/src/ru/shishmakov/StarShooter.java
@@ -7,13 +7,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	 SpriteBatch batch;
+	 Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("fon.jpg");
+		img = new Texture("fon_hw.jpg");
 
 	}
 
