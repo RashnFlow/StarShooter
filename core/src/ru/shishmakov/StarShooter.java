286c74aed622d0b11208dc802e2f5d231436d380
Ilya <rashnflow@yandex.ru>
2020-04-30 14:57:14 +0300

    homework2 Перемещается только при 1 нажатии, дальше он не хочет нормально работать :(

--- core/src/ru/shishmakov/StarShooter.java
+++ core/src/ru/shishmakov/StarShooter.java
@@ -1,34 +1,13 @@
 package ru.shishmakov;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class StarShooter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import ru.shishmakov.screen.MenuScreen;
+
+public class StarShooter extends Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("fon.jpg");
-
+		setScreen( new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0.5f, 0.12f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
